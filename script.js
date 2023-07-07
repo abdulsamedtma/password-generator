ec55f8466520dd25ad4ce389029b207d49babc7a
Abdul Samed Alhassan <129804994+abdulsamedtma@users.noreply.github.com>
2023-07-06 21:37:00 -0500

    initial commit

--- script.js
+++ script.js
@@ -1,76 +1,75 @@
 //   Pseudo code
 // 1. Prompt the user for the password criteria
-// i. Password length 8 < 128 characters
-// ii. Lowercase, uppercase, numbers, special characters
-// 2.  Validate the input
-// 3.  Generate password based on criteria
-// 4. Display password on the page.
+  // i. Password length 8 < 128 characters
+  // ii. Lowercase, uppercase, numbers, special characters
+  // 2.  Validate the input
+  // 3.  Generate password based on criteria
+  // 4. Display password on the page.
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+    passwordText.value = password;
 
 }
 //Function to generate random password
 function generatePassword() {
-  //Define charater set for different type of characters
-  const uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  const lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
-  const numericChars = "0123456789";
-  const specialChars = "@#!$%^&*()_+\-=[]{}~;':><.?/`";
+    //Define charater set for different type of characters
+    const uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+    const numericChars = "0123456789";
+    const specialChars = "@#!$%^&*()_+\-=[]{}~;':><.?/`";
 
-  //Ask for the password length and validate the input
-  let passwordLength = 0;
-  while (passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength)) {
-    passwordLength = parseInt(
-      prompt("Please enter your desire password length (8 to 128 characters):")
-    );
-  }
+    //Ask for the password length and validate the input
+    let passwordLength = 0;
+    while (passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength)) {
+        passwordLength = parseInt(
+            prompt("Please enter your desire password length (8 to 128 characters):")
+        );
+    }
 
-  // Ask for character types to be include in password
-  const includeUpppercase = confirm("Include uppercase letters?");
-  const includeLowercase = confirm("Include lowercase letters?");
-  const includenumeric = confirm("Include numeric characters?");
-  const includeSpecial = confirm("Include special characters?");
+    // Ask for character types to be include in password
+    const includeUpppercase = confirm("Include uppercase letters?");
+    const includeLowercase = confirm("Include lowercase letters?");
+    const includenumeric = confirm("Include numeric characters?");
+    const includeSpecial = confirm("Include special characters?");
 
-  // Validate that atleast one charater type is selection 
-  if (
-    !includeUpppercase &&
-    !includeLowercase &&
-    !includenumeric &&
-    !includeSpecial
-  ) {
-    alert("Please You must select at least one character type.");
-    return;
-  }
-  // Generate the charaters set based on the user selected charater type 
-  let characterSet = "";
-  if (includeUpppercase) characterSet += uppercaseChars;
-  if (includeLowercase) characterSet += lowercaseChars;
-  if (includeUpppercase) characterSet += numericChars;
-  if (includeSpecial) characterSet += specialChars;
-  // Generate the Password
-  let password = "";
-  for (let i = 0; i < passwordLength; i++) {
+    // Validate that atleast one charater type is selection 
+    if (
+        !includeUpppercase&&
+        !includeLowercase &&
+        !includenumeric &&
+        !includeSpecial
+      ) {
+        alert("Please You must select at least one character type.");
+        return;
+      }
+// Generate the charaters set based on the user selected charater type 
+let characterSet = "";
+if (includeUpppercase) characterSet += uppercaseChars;
+if (includeLowercase) characterSet += lowercaseChars;
+if (includeUpppercase) characterSet += numericChars;
+if (includeSpecial) characterSet += specialChars;
+// Generate the Password
+let password = "";
+for (let i =0; i < passwordLength; i++) {
     const randomIndex = Math.floor(Math.random() * characterSet.length);
     password += characterSet[randomIndex];
-  }
-  return password;
+}
+return password;
 }
 
 // Funtion to update the password textarea with the generated password
-function updatePassword() {
-  const generatedPassword = generatedPassword();
-  const passwordTextArea = document.getElementById("password");
-  passwordTextArea.value = generatedPassword;
+function updatePassword(){
+    const generatedPassword= generatedPassword();
+    const passwordTextArea = document.getElementById("password");
+    passwordTextArea.value = generatedPassword;
 }
-// Add event listener to generate button
-const generatebutton = document.getElementById("generate");
-generatebutton.addEventListener("click", writePassword);
-
+    // Add event listener to generate button
+    const generatebutton = document.getElementById("generate");
+    generatebutton.addEventListener("click", writePassword);
